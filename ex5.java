f824dc24b6b8146de5918774944c75f06135b5b9
LeonardoCZanin <leonardocruzzanin@gmail.com>
2026-02-28 18:58:46 -0300

    Update ex5.java

--- ex5.java
+++ ex5.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
+import java.util.Random;
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner sc =  new Scanner(System.in);
+        Random rand = new Random();
+        int numeroGerado=rand.nextInt(10)+1;
+        int resposta;
 
+        System.out.println("qual o numero aleatorio foi gerado? ");
+        do {
+             resposta = sc.nextInt();
+        }while(resposta<0 && resposta<10);
+
+        while(resposta!=numeroGerado)
+        {
+            System.out.println("Errou o numero insira de novo");
+            resposta = sc.nextInt();
+        };
+
+        System.out.println("Acertou o numero");
+
+
+        sc.close();
+    }
+
+}
