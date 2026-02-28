f3a9034f6abacf9206642308ca1f81a8ccf2d482
LeonardoCZanin <leonardocruzzanin@gmail.com>
2026-02-28 16:13:27 -0300

    Update ex2.java

--- ex2.java
+++ ex2.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main (String[] args)
+{
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int c = sc.nextInt();
+    String d = String.format("%.2f",((a+b+c)/3.0));
+    System.out.println("Lanches consumidos ao total : " + (a+b+c));
+    System.out.println("Lanches consumidos em media : "+ d);
+    sc.close();
 
+}
+}
