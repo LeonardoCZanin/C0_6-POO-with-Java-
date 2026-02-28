e2ac788df84101d842d2ae64bea9353090502115
LeonardoCZanin <leonardocruzzanin@gmail.com>
2026-02-28 19:08:30 -0300

    Update EX6.java

--- EX6.java
+++ EX6.java
@@ -1,1 +1,38 @@
+import java.util.Scanner;
+import java.util.Random;
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner sc =  new Scanner(System.in);
+        Random rand = new Random();
+        int numeroGerado=rand.nextInt(10)+1;
+        int resposta;
+        int tentativasNecessarias=0;
+
+        System.out.println("qual o numero aleatorio foi gerado? ");
+        do {
+             resposta = sc.nextInt();
+             tentativasNecessarias++;
+        }while(resposta<0 && resposta<10);
+
+        while(resposta!=numeroGerado)
+        {
+            System.out.println("Errou o numero insira de novo");
+            if(resposta>numeroGerado) {
+                System.out.println("sabendo que o numero gerado e menor que a resposta anterior ");
+            }
+            else {
+                System.out.println("sabendo que o numero gerado e maior que a resposta anterior");
+            }
+            tentativasNecessarias++;
+            resposta = sc.nextInt();
+        };
+
+        System.out.println("Acertou o numero");
+        System.out.println("tentativas necessarias : "+tentativasNecessarias);
+
+        sc.close();
+    }
+
+}
 
