1b8a3fda5e33c68e1e0e3c3f4148e79f2bfd4b9d
LeonardoCZanin <leonardocruzzanin@gmail.com>
2026-02-28 17:02:38 -0300

    Update ex3.java

--- ex3.java
+++ ex3.java
@@ -1,16 +1,30 @@
 import java.util.Scanner;
-public class Main
-{
-    public static void main (String[] args)
-{
-    Scanner sc = new Scanner(System.in);
-    int a = sc.nextInt();
-    int b = sc.nextInt();
-    int c = sc.nextInt();
-    String d = String.format("%.2f",((a+b+c)/3.0));
-    System.out.println("Lanches consumidos ao total : " + (a+b+c));
-    System.out.println("Lanches consumidos em media : "+ d);
-    sc.close();
 
-}
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Insira a nota da prova: ");
+        double notaprova = sc.nextDouble();
+
+        if (notaprova >= 30 && notaprova < 60) {
+            System.out.println("Aluno ficou de recuperacao");
+            System.out.println("Insira a nota obtida na recuperacao: ");
+            double recuperacao = sc.nextDouble();
+
+            double media = (notaprova + recuperacao) / 2;
+            if (media < 50) {
+                System.out.println("Aluno  reprovado");
+            } else {
+                System.out.println("Aluno foi aprovado");
+            }
+
+        } else if (notaprova >= 60) {
+            System.out.println("Aluno foi aprovado");
+        } else {
+            System.out.println("Aluno foi reprovado");
+        }
+
+        sc.close();
+    }
 }
