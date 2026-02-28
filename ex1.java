565fb7150db635b0e1ba518ac23009c3428903aa
LeonardoCZanin <leonardocruzzanin@gmail.com>
2026-02-28 15:43:35 -0300

    Update ex1.java

--- ex1.java
+++ ex1.java
@@ -1,1 +1,13 @@
-
+public class Main
+{
+    public static void main(String[] args)
+    {
+       int a = 10;
+       int b = 4;
+       int c = 2;
+       double d = (a+b+c)/3.0;
+       String media = String.format("%.2f",d);
+        System.out.println("Numero total de lanches consumidos : "+(a+b+c));
+        System.out.println( "Media de lanches consumidos : " + media );
+    }
+}
